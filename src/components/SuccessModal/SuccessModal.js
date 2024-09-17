58787373ade50b92e298b6a2613c2ec2d2f0f5e4
Aman Tanwar <aman16tanwar@gmail.com>
2024-09-16 22:58:52 -0700

    Success Modal added 3

--- src/components/SuccessModal/SuccessModal.js
+++ src/components/SuccessModal/SuccessModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaCheckCircle} from 'react-icons/fa';
+import { FaCheckCircle } from 'react-icons/fa';
 
 const SuccessModal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
